633334c4745d1625f8b43c3a0c763e150e9aaf3f
UltronTheDeath3000 <arina@pi.local>
2017-06-29 21:09:23 +0300

    HW 29.06
    Genome

---
+++ src/main/java/HWLesson4/GeneratCode.java
@@ -0,0 +1,30 @@
+package HWLesson4;
+
+import java.util.Random;
+
+/**
+ * Created by arina on 29.06.17.
+ */
+public class GeneratCode {
+
+        static final byte[] ALPHABET = {'A', 'C', 'T', 'G'};
+
+        public static byte[] generate(){
+
+            byte[] code = new byte[30];
+            Random random = new Random();
+
+            for (int i = 0; i < code.length ; i++) {
+                code[i] = ALPHABET[random.nextInt(4)];
+                System.out.print((char) code[i] + " ");
+            }
+            System.out.println();
+
+            return code;
+        }
+
+//    public static void main(String[] args) {
+//        generate();
+//    }
+
+}
---
+++ src/main/java/HWLesson4/Genome.java
@@ -0,0 +1,40 @@
+package HWLesson4;
+
+/**
+ * Created by arina on 29.06.17.
+ */
+public class Genome {
+
+    public static void rework(byte[] code, int m){
+        for (int i = 0; i < code.length - m ; i++) {
+            for (int j = i + 1; j < code.length - m; j++) {
+                boolean flag = false;
+
+               INNER: for (int k = 0; k < m ; k ++) {
+                    if(code[j] == code[i]){
+                        flag = true; // если совпадает
+
+                        j++;
+                        i++;
+                        if(k == m-1){
+                            for (int l = m; l > 0; l--) {
+                                System.out.print((char) code[i - l]);
+                            }
+                            System.out.print(" " + i + " " + j +  ", ");
+                        }
+                        continue INNER;
+
+
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        byte[] code = GeneratCode.generate();
+
+        rework(code, 3);
+    }
+}
