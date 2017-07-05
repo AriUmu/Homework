17511060679d83c2d4f82fc9b6d67feeba3a5082
UltronTheDeath3000 <arina@pi.local>
2017-07-05 11:56:03 +0300

    5.07.2017
    Array toString using «for»

--- src/main/java/HWLesson6/PrintArray.java
+++ src/main/java/HWLesson6/PrintArray.java
@@ -1,7 +1,94 @@
 package HWLesson6;
 
+import java.util.Arrays;
+
 /**
  * Created by arina on 04.07.17.
  */
 public class PrintArray {
+    public static void main(String[] args) {
+
+        int a[] = {1,2,3,4,5};
+        int[] b = {};
+        byte[] c = {2,-3,4};
+        short[] s = {127, 56, 0};
+        double[] d = {123.5, 67.8754, 34.5677, 0,543, -34,3};
+        float[] f = {123.5f, 67.4f, 34.877f, 0, 543, -34,3};
+        String [] str = {"lol","kek", "cheburek"};
+
+        printArray(a);
+        printArray(b);
+        printArray(str);
+        printArray(c);
+        printArray(s);
+        printArray(d);
+        printArray(f);
+
+
+        Arrays.toString(a);
+    }
+
+    public static String printArray (int[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
+
+    public static String printArray (char[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
+
+
+    public static String printArray (short[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
+
+    public static String printArray (byte[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
+
+    public static String printArray (double[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
+    public static String printArray (float[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
+
+
+    public static String printArray (Object[] o){
+        System.out.print("[");
+        for (int i = 0; i <o.length ; i++) {
+            System.out.print(o[i] + " ");
+        }
+        System.out.println("]");
+        return "";
+    }
 }
